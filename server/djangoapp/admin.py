5517d3a007c6eee3822bd6a01dfd7815c5c8efe0
sitangshu <sitangshu.chand@outlook.com>
2023-10-15 10:31:51 -0400

    added carmake and carmodel

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
-from .models import CarModel,CarMake
+from .models import (CarModel,CarMake)
 
 
 # Register your models here.
-
+# admin.site.register(CarModel)
+# admin.site.register(CarMake)
 # CarModelInline class
-admin.site.register(CarModel)
-admin.site.register(CarMake)
+class CarModelInline(admin.TabularInline):
+    model=[CarModel,CarMake]
 # CarModelAdmin class
-
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['delerid', 'name']
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ['name', 'description']
 
 # Register models here
+admin.site.register(CarModel,CarModelAdmin)
+admin.site.register(CarMake,CarMakeAdmin)
